999279b968d978aad470492bd47104485b553186
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-02 22:34:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,5 @@ def fizzbuzz(num)
   "Fizz" # Go fizz
 end
 
-require_relative './spec_helper.rb'
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
 
-    fizz_3 = "fizzbuzz(3)"
-    expect(fizz_3).to eq("Fizz")
 end
