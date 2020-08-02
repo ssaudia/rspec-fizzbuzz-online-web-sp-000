0f3fca7334874b93d32ee2e982307958b69049f6
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-02 22:29:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,6 +7,6 @@ require_relative './spec_helper.rb'
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
 
-    fizz_3= "fizzbuzz(3)"
+    fizz_3 = "fizzbuzz(3)"
     expect(fizz_3).to eq("Fizz")
 end
