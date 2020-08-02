48db3d49f58b38f054a1c44ecabb09a10fe7eb29
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-02 22:45:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 def fizzbuzz(num)
-  if num % 3 == 0 # if the number int is divisible by 3
+if num % 3
+elsif num % 3 == 0 # if the number int is divisible by 3
   "Fizz" # Go fizz
 elsif num % 5 == 0
   "Buzz"
